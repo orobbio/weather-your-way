e3880322a60205d98909e0cd2d86a4963a370031
Aaron J Mitchell <amitchell1992@knights.ucf.edu>
2019-11-26 19:23:50 -0500

    firebase added, button div added

--- assets/js/wyw.js
+++ assets/js/wyw.js
@@ -1,3 +1,42 @@
+var city = "";
+var cityList = [];
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA_HKdaGvOZxcYv5ODVcDwxqaxWSRETSeA",
+    authDomain: "wyw-test-5a215.firebaseapp.com",
+    databaseURL: "https://wyw-test-5a215.firebaseio.com",
+    projectId: "wyw-test-5a215",
+    storageBucket: "wyw-test-5a215.appspot.com",
+    messagingSenderId: "116809492534",
+    appId: "1:116809492534:web:cc275ff07dca9f4e829097"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+database.ref().update({
+    city: city,
+    cityList: cityList
+});
+
+function createButtons() {
+
+    database.ref().on("value", function (snapshot) {
+        cityList = snapshot.val().cityList;
+        console.log(cityList);
+        $("#buttonRow").empty();
+        for (var i = 0; i < cityList.length; i++) {
+            var cityButton = $("<button>");
+            cityButton.attr("id", "city-name");
+            cityButton.addClass("btn btn-primary");
+            cityButton.attr("data-name", cityList[i]);
+            cityButton.text(cityList[i]);
+            $("#buttonRow").append(cityButton);
+        }
+    })
+}
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('mapInfo'), {
         center: {
@@ -57,11 +96,11 @@ function getWeather() {
 
         var tbody = $("<tbody>");
         extended.append(tbody);
-        
 
-        for(var i = 1; i < 6; i++){
+
+        for (var i = 1; i < 6; i++) {
             var newHR = $("<tr>");
-            
+
             var HRTime = $("<td>").text(moment.unix(response.hourly.data[i].time).format("LT"));
             var HRTemp = $("<td>").text(Math.round(response.hourly.data[i].temperature) + "°");
             var HRIcon = $("<img>");
@@ -71,7 +110,7 @@ function getWeather() {
             HRIcon.attr("height", "50px");
 
 
-            
+
             newHR.append(HRTime, HRTemp, HRIcon);
             tbody.append(newHR);
 
@@ -89,10 +128,54 @@ function getWeather() {
 var lat = 0;
 var lng = 0;
 
+$(document).on("click", "#city-name", buttonWeather);
+
+
+function buttonWeather() {
+
+    city = $(this).attr("data-name");
+
+    $("#traffic").text("Today's Traffic Conditions in " + city);
+    $("#weather").text("Weather forcast in " + city);
+
+    var queryURL =
+        "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=AIzaSyB5dx1l-uXDBY9Yg8GpMePGvo_QV_VoF1c"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log(response.results[0].geometry.location);
+        lat = response.results[0].geometry.location.lat;
+        lng = response.results[0].geometry.location.lng;
+        console.log(lat);
+    }).then(function () {
+
+        getWeather();
+
+    }).then(function () {
+
+        var map;
+        initMap();
+    });
+
+};
+
 $("#addCity").on("click", function (event) {
-    var city = $("#cityText").val().trim();
+    city = $("#cityText").val().trim();
+    cityList.push(city);
+
+    database.ref().update({
+        city: city,
+        cityList: cityList
+    });
+
+    $("#traffic").text("Today's Traffic Conditions in " + city);
+    $("#weather").text("Weather forcast in " + city);
 
     $("#cityText").val("");
+    createButtons();
 
     var queryURL =
         "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=AIzaSyB5dx1l-uXDBY9Yg8GpMePGvo_QV_VoF1c"
